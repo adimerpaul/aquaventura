442b4ea7dccdb562ed377e331ebaf3af7b49c6a1
adimerpaul <adimer101@gmail.com>
2022-12-29 01:02:07 -0400

    creado una version con pwa

--- front/quasar.config.js
+++ front/quasar.config.js
@@ -91,7 +91,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api/'
-          : 'https://prod.api.com'
+          : 'https://baquaventura.siscenter.org/api/'
       }
     },
 
